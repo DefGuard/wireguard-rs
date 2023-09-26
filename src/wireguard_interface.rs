7112808d9a24c94d4f721d3b19720c16c59a3f81
Maciej Wójcik <wojcik91@gmail.com>
2023-09-26 11:18:05 +0200

    add return values

--- src/wireguard_interface.rs
+++ src/wireguard_interface.rs
@@ -12,18 +12,21 @@ pub trait WireguardInterfaceApi {
     fn assign_address(&self, addr: &IpAddrMask) -> Result<(), WireguardInterfaceError>;
 
     /// Updates configuration of an existing WireGuard interface.
-    fn configure_interface(&self, config: &InterfaceConfiguration);
+    fn configure_interface(
+        &self,
+        config: &InterfaceConfiguration,
+    ) -> Result<(), WireguardInterfaceError>;
 
     /// Removes the WireGuard interface being managed.
     ///
     /// Meant to be used in `drop` method for a given API struct.
-    fn remove_interface(&self);
+    fn remove_interface(&self) -> Result<(), WireguardInterfaceError>;
 
     /// Adds a peer or updates peer configuration.
-    fn configure_peer(&self, peer: &Peer);
+    fn configure_peer(&self, peer: &Peer) -> Result<(), WireguardInterfaceError>;
 
     /// Removes a configured peer with a given pubkey.
-    fn remove_peer(&self, peer_pubkey: &str);
+    fn remove_peer(&self, peer_pubkey: &str) -> Result<(), WireguardInterfaceError>;
 
     /// Reads current WireGuard interface configuration and stats.
     ///
