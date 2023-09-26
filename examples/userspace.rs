3745b236333d6827c719a2c3bb88ac3c43e2be10
Maciej Wójcik <wojcik91@gmail.com>
2023-09-26 13:46:42 +0200

    try out a fix

--- examples/userspace.rs
+++ examples/userspace.rs
@@ -33,7 +33,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     log::info!("endpoint");
     // Your wireguard server endpoint which peer connects too
-    let endpoint: SocketAddr = "<server_ip>:<server_port>".parse().unwrap();
+    let endpoint: SocketAddr = "10.20.30.40:55001".parse().unwrap();
     // Peer endpoint and interval
     peer.endpoint = Some(endpoint);
     peer.persistent_keepalive_interval = Some(25);
@@ -43,27 +43,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     for allowed_ip in allowed_ips {
         let addr = IpAddrMask::from_str(allowed_ip)?;
         peer.allowed_ips.push(addr);
-        // Add a route for the allowed IP using the `ip -4 route add` command
-        let output = std::process::Command::new("ip")
-            .args(&["-4", "route", "add", allowed_ip, "dev", "wg0"])
-            .output()?;
-
-        if output.status.success() {
-            log::info!("Added route for {}", allowed_ip);
-        } else {
-            log::error!("Failed to add route for {}: {:?}", allowed_ip, output);
-        }
     }
 
     // interface configuration
-    let prvkey: Key = StaticSecret::random()
-        .to_bytes()
-        .as_ref()
-        .try_into()
-        .unwrap();
     let interface_config = InterfaceConfiguration {
         name: ifname.clone(),
-        prvkey: prvkey.to_lower_hex(),
+        prvkey: "AAECAwQFBgcICQoLDA0OD/Dh0sO0pZaHeGlaSzwtHg8=".to_string(),
         address: "10.6.0.30".to_string(),
         port: 12345,
         peers: vec![peer],
