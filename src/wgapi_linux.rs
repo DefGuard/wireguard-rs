aa2e710b642dd4420eee816337342d9382cf621d
Maciej Wójcik <wojcik91@gmail.com>
2023-09-27 13:00:05 +0200

    implement dedicated linux API

--- src/wgapi_linux.rs
+++ src/wgapi_linux.rs
@@ -1,7 +1,8 @@
 use crate::{
-    Host, InterfaceConfiguration, IpAddrMask, Key, Peer, WireguardInterfaceApi,
+    netlink, Host, InterfaceConfiguration, IpAddrMask, Key, Peer, WireguardInterfaceApi,
     WireguardInterfaceError,
 };
+use std::str::FromStr;
 
 /// Manages interfaces created with Linux kernel WireGuard module.
 ///
@@ -19,33 +20,63 @@ impl WireguardApiLinux {
 
 impl WireguardInterfaceApi for WireguardApiLinux {
     fn create_interface(&self) -> Result<(), WireguardInterfaceError> {
-        todo!()
+        info!("Creating interface {}", self.ifname);
+        netlink::create_interface(&self.ifname)?;
+        Ok(())
     }
 
-    fn assign_address(&self, addr: &IpAddrMask) -> Result<(), WireguardInterfaceError> {
-        todo!()
+    fn assign_address(&self, address: &IpAddrMask) -> Result<(), WireguardInterfaceError> {
+        debug!("Assigning address {address} to interface {}", self.ifname);
+        netlink::address_interface(&self.ifname, address)?;
+        Ok(())
     }
 
     fn configure_interface(
         &self,
         config: &InterfaceConfiguration,
     ) -> Result<(), WireguardInterfaceError> {
-        todo!()
+        info!(
+            "Configuring interface {} with config: {config:?}",
+            self.ifname
+        );
+        // create interface
+        self.create_interface()?;
+
+        // assign IP address to interface
+        let address = IpAddrMask::from_str(&config.address)?;
+        self.assign_address(&address)?;
+
+        // configure interface
+        let host = config.try_into()?;
+        netlink::set_host(&self.ifname, &host)?;
+
+        Ok(())
     }
 
     fn remove_interface(&self) -> Result<(), WireguardInterfaceError> {
-        todo!()
+        info!("Removing interface {}", self.ifname);
+        netlink::delete_interface(&self.ifname)?;
+        Ok(())
     }
 
     fn configure_peer(&self, peer: &Peer) -> Result<(), WireguardInterfaceError> {
-        todo!()
+        info!("Configuring peer {peer:?} on interface {}", self.ifname);
+        netlink::set_peer(&self.ifname, peer)?;
+        Ok(())
     }
 
     fn remove_peer(&self, peer_pubkey: &Key) -> Result<(), WireguardInterfaceError> {
-        todo!()
+        info!(
+            "Removing peer with public key {peer_pubkey} from interface {}",
+            self.ifname
+        );
+        netlink::delete_peer(&self.ifname, peer_pubkey)?;
+        Ok(())
     }
 
     fn read_interface_data(&self) -> Result<Host, WireguardInterfaceError> {
-        todo!()
+        debug!("Reading host info for interface {}", self.ifname);
+        let host = netlink::get_host(&self.ifname)?;
+        Ok(host)
     }
 }
