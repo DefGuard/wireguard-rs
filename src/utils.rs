0ffe177f6a93148ca3ddb7f514738b09dc395edb
dzania <artur.kantorczyk@gmail.com>
2023-11-21 12:19:18 +0100

    add more logs

--- src/utils.rs
+++ src/utils.rs
@@ -68,63 +68,63 @@ pub(crate) fn add_peers_routing(
 
             // Add table rules
             debug!("Adding route for allowed IP: {}", allowed_ip);
-            let output = Command::new("ip")
-                .args([
-                    proto,
-                    "route",
-                    "add",
-                    &allowed_ip,
-                    "dev",
-                    ifname,
-                    "table",
-                    &fwmark.to_string(),
-                ])
-                .output()?;
+            let args = [
+                proto,
+                "route",
+                "add",
+                &allowed_ip,
+                "dev",
+                ifname,
+                "table",
+                &fwmark.to_string(),
+            ];
+            debug!("Executing command `ip` with args: {:?}", args);
+            let output = Command::new("ip").args(args).output()?;
             check_command_output_status(output)?;
 
             debug!("Adding rule for fwmark: {}", fwmark);
-            let output = Command::new("ip")
-                .args([
-                    proto,
-                    "rule",
-                    "add",
-                    "not",
-                    "fwmark",
-                    &fwmark.to_string(),
-                    "table",
-                    &fwmark.to_string(),
-                ])
-                .output()?;
+            let args = [
+                proto,
+                "rule",
+                "add",
+                "not",
+                "fwmark",
+                &fwmark.to_string(),
+                "table",
+                &fwmark.to_string(),
+            ];
+            debug!("Executing command `ip` with args: {:?}", args);
+            let output = Command::new("ip").args(args).output()?;
             check_command_output_status(output)?;
 
             debug!("Adding rule for main table");
-            let output = Command::new("ip")
-                .args([
-                    proto,
-                    "rule",
-                    "add",
-                    "table",
-                    "main",
-                    "suppress_prefixlength",
-                    "0",
-                ])
-                .output()?;
+            let args = [
+                proto,
+                "rule",
+                "add",
+                "table",
+                "main",
+                "suppress_prefixlength",
+                "0",
+            ];
+            debug!("Executing command `ip` with args: {:?}", args);
+            let output = Command::new("ip").args(args).output()?;
             check_command_output_status(output)?;
 
             if is_ipv6 {
                 debug!("Reloading ip6tables");
-                debug!("Running ip6tables-restore -n");
+                debug!("Executing ip6tables-restore -n");
                 let output = Command::new("ip6tables-restore").arg("-n").output()?;
                 check_command_output_status(output)?;
             } else {
-                debug!("Setting systemctl net.ipv4.conf.all.src_valid_mark=1");
+                debug!("executing `sysctl -q systemctl net.ipv4.conf.all.src_valid_mark=1`");
                 let output = Command::new("sysctl")
                     .args(["-q", "net.ipv4.conf.all.src_valid_mark=1"])
                     .output()?;
                 check_command_output_status(output)?;
 
                 debug!("Reloading iptables");
-                debug!("Running iptables-restore -n");
+                debug!("Executing `iptables-restore -n`");
                 let output = Command::new("iptables-restore").arg("-n").output()?;
                 check_command_output_status(output)?;
             }
@@ -132,13 +132,13 @@ pub(crate) fn add_peers_routing(
             // Normal routing
             let args = [proto, "route", "add", &allowed_ip, "dev", ifname];
             debug!("Adding route for allowed IP: {}", allowed_ip);
-            debug!("Running command ip {:?}", args);
+            debug!("Executing command `ip` with args: {:?}", args);
             let output = Command::new("ip").args(args).output()?;
             check_command_output_status(output)?;
         }
     }
 
-    debug!("Peers routing added successfully");
+    info!("Peers routing added successfully");
     Ok(())
 }
 
@@ -147,8 +147,10 @@ pub(crate) fn add_peers_routing(
     peers: &[Peer],
     ifname: &str,
 ) -> Result<(), WireguardInterfaceError> {
+    debug!("Adding peers routing for interface: {}", ifname);
     let mut unique_allowed_ips = HashSet::new();
     let mut endpoints = HashSet::new();
+
     for peer in peers {
         if let Some(endpoint) = peer.endpoint {
             endpoints.insert(endpoint);
@@ -158,42 +160,47 @@ pub(crate) fn add_peers_routing(
         }
     }
     for allowed_ip in unique_allowed_ips {
+        debug!("Processing allowed IP: {}", allowed_ip);
         let is_ipv6 = allowed_ip.contains(':');
         let (proto, route1, route2) = match is_ipv6 {
             true => ("-inet6", "::/1", "8000::/1"),
             false => ("-inet", "0.0.0.0/1", "128.0.0.0/1"),
         };
         if ["0.0.0.0/0".to_string(), "::/0".to_string()].contains(&allowed_ip) {
+            debug!("Processing default route: {}", allowed_ip);
             // Add table rules
-            Command::new("route")
-                .args(["-q", "-n", "add", proto, route1, "-interface", ifname])
-                .output()?;
-            Command::new("route")
-                .args(["-q", "-n", "add", proto, route2, "-interface", ifname])
-                .output()?;
+            let args = ["-q", "-n", "add", proto, route1, "-interface", ifname];
+            debug!("Executing command `route` with args: {:?}", args);
+            let output = Command::new("route").args(args).output()?;
+            check_command_output_status(output)?;
+            let args = ["-q", "-n", "add", proto, route2, "-interface", ifname];
+            debug!("Executing command `route` with args: {:?}", args);
+            let output = Command::new("route").args(args).output()?;
+            check_command_output_status(output)?;
             // route endpoints
             for endpoint in &endpoints {
+                debug!("Processing endpoint: {}", endpoint);
                 let (ip_version, proto) = match endpoint.is_ipv4() {
                     true => (IpVersion::IPv4, "-inet"),
                     false => (IpVersion::IPv6, "-inet6"),
                 };
                 let gateway = collect_gateway(ip_version)?;
                 // Precautionary route delete don't handle result because it may not exist
-                let output = Command::new("route")
-                    .args(["-q", "-n", "delete", proto, &endpoint.ip().to_string()])
-                    .output();
+                let args = ["-q", "-n", "delete", proto, &endpoint.ip().to_string()];
+                debug!("Executing command `route` with args {:?}", args);
+                let _ = Command::new("route").args(args).output();
                 if !gateway.is_empty() {
-                    let output = Command::new("route")
-                        .args([
-                            "-q",
-                            "-n",
-                            "add",
-                            proto,
-                            &endpoint.ip().to_string(),
-                            "-gateway",
-                            &gateway,
-                        ])
-                        .output()?;
+                    let args = [
+                        "-q",
+                        "-n",
+                        "add",
+                        proto,
+                        &endpoint.ip().to_string(),
+                        "-gateway",
+                        &gateway,
+                    ];
+                    debug!("Executing command `route` with args {:?}", args);
+                    let output = Command::new("route").args(args).output()?;
                     check_command_output_status(output)?;
                 } else {
                     // Prevent routing loop as in wg-quick
@@ -201,27 +208,28 @@ pub(crate) fn add_peers_routing(
                         true => "127.0.0.1",
                         false => "::1",
                     };
-                    let output = Command::new("route")
-                        .args([
-                            "-q",
-                            "-n",
-                            "add",
-                            proto,
-                            &endpoint.ip().to_string(),
-                            address,
-                            "-blackhole",
-                        ])
-                        .output()?;
+                    let args = [
+                        "-q",
+                        "-n",
+                        "add",
+                        proto,
+                        &endpoint.ip().to_string(),
+                        address,
+                        "-blackhole",
+                    ];
+                    debug!("Executing command `route` with args {:?}", args);
+                    let output = Command::new("route").args(args).output()?;
                     check_command_output_status(output)?;
                 }
             }
         } else {
-            let output = Command::new("route")
-                .args(["-q", "-n", "add", proto, &allowed_ip, "-interface", ifname])
-                .output()?;
+            let args = ["-q", "-n", "add", proto, &allowed_ip, "-interface", ifname];
+            debug!("Executing command `route` with args {:?}", args);
+            let output = Command::new("route").args(args).output()?;
             check_command_output_status(output)?;
         }
     }
+    info!("Peers routing added successfully");
     Ok(())
 }
 
@@ -237,10 +245,12 @@ pub(crate) enum IpVersion {
 /// Needed to add proper routing for 0.0.0.0/0, ::/0
 #[cfg(any(target_os = "macos", target_os = "freebsd"))]
 pub(crate) fn collect_gateway(ip_version: IpVersion) -> Result<String, WireguardInterfaceError> {
+    debug!("Collecting gateway.");
     let command_args = match ip_version {
         IpVersion::IPv4 => &["-nr", "-f", "inet"],
         IpVersion::IPv6 => &["-nr", "-f", "inet6"],
     };
+    debug!("Executing command `netstat` with args: {:?}", command_args);
 
     let output = Command::new("netstat").args(command_args).output()?;
 
@@ -248,10 +258,12 @@ pub(crate) fn collect_gateway(ip_version: IpVersion) -> Result<String, Wireguard
 
     for line in output_str.lines() {
         let fields: Vec<&str> = line.split_whitespace().collect();
+        debug!("Processing output line: {:?}", fields);
         if fields.len() > 1 && fields[0] == "default" && !fields[1].contains("link#") {
             return Ok(fields[1].to_string());
         }
     }
+    error!("Gateway not found");
 
     Ok(String::new())
 }
@@ -266,17 +278,17 @@ pub(crate) fn collect_gateway(ip_version: IpVersion) -> Result<String, Wireguard
 /// ip -6 rule delete table main suppress_prefixlength 0
 #[cfg(target_os = "linux")]
 pub(crate) fn clean_fwmark_rules(fwmark: &str) -> Result<(), WireguardInterfaceError> {
+    debug!("Removing fwmark rules.");
     for ip_type in ["-4", "-6"] {
         // Check if rule exists `ip <ip_type> rule show`
-        let ip_rules = Command::new("ip")
-            .args([ip_type, "rule", "show"])
-            .output()?
-            .stdout;
+        let args = [ip_type, "rule", "show"];
+        debug!("Executing command `ip` with args: {:?}", args);
+        let ip_rules = Command::new("ip").args(args).output()?.stdout;
         // Check ip rules contains fwmark rules if yes run `ip <ip_type> rule delete table fwmark`
         if String::from_utf8_lossy(&ip_rules).contains(&format!("lookup {}", fwmark)) {
-            let output = Command::new("ip")
-                .args([ip_type, "rule", "delete", "table", fwmark])
-                .output()?;
+            let args = [ip_type, "rule", "delete", "table", fwmark];
+            debug!("Executing command `ip` with args: {:?}", args);
+            let output = Command::new("ip").args(args).output()?;
             check_command_output_status(output)?;
         };
         // Check ip rules contains suppress lookup if yes delete using:
@@ -284,17 +296,17 @@ pub(crate) fn clean_fwmark_rules(fwmark: &str) -> Result<(), WireguardInterfaceE
         if String::from_utf8_lossy(&ip_rules)
             .contains("from all lookup main suppress_prefixlength 0")
         {
-            let output = Command::new("ip")
-                .args([
-                    ip_type,
-                    "rule",
-                    "delete",
-                    "table",
-                    "main",
-                    "suppress_prefixlength",
-                    "0",
-                ])
-                .output()?;
+            let args = [
+                ip_type,
+                "rule",
+                "delete",
+                "table",
+                "main",
+                "suppress_prefixlength",
+                "0",
+            ];
+            debug!("Executing command `ip` with args: {:?}", args);
+            let output = Command::new("ip").args(args).output()?;
             check_command_output_status(output)?;
         };
     }
