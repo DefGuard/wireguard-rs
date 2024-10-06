bf23aff2b64abc9ff98bec91b1b411e5eb67f350
Aleksander <170264518+t-aleksander@users.noreply.github.com>
2024-10-06 21:42:53 +0200

    don't invoke commands to test if they exist

--- src/dependencies.rs
+++ src/dependencies.rs
@@ -1,38 +1,54 @@
-use std::process::Command;
+use std::env;
 
 use crate::error::WireguardInterfaceError;
 
 #[cfg(target_os = "linux")]
-const COMMANDS: [(&str, &str); 2] = [("resolvconf", "--version"), ("ip", "help")];
+const COMMANDS: [&str; 2] = ["resolvconf", "ip"];
 
-// There is no Windows command to check the version of WireGuard.
-// The "/" argument (or any other non-existent argument) normally returns a help message popup. However when invoked
-// by means of rust's std::process::Command, it only results in an `Ok` output (or an `Err` if the command is not found),
-// allowing us to check if the command is available.
 #[cfg(target_os = "windows")]
-const COMMANDS: [(&str, &str); 1] = [("wireguard", "/")];
+const COMMANDS: [&str; 1] = [("wireguard.exe")];
 
 #[cfg(target_os = "macos")]
-const COMMANDS: [(&str, &str); 2] = [("wireguard-go", "--version"), ("networksetup", "-version")];
+const COMMANDS: [&str; 2] = ["wireguard-go", "networksetup"];
 
 #[cfg(any(target_os = "freebsd", target_os = "netbsd"))]
-const COMMANDS: [(&str, &str); 1] = [("resolvconf", "--version")];
+const COMMANDS: [&str; 1] = ["resolvconf"];
 
-/// Check if the commands/executables required for interface management are available.
 pub(crate) fn check_external_dependencies() -> Result<(), WireguardInterfaceError> {
-    for (cmd, arg) in COMMANDS.iter() {
-        debug!(
-            "Checking if command `{}` is available by running: {} {}",
-            cmd, cmd, arg
-        );
-        Command::new(cmd).arg(arg).output().map_err(|err| {
-            WireguardInterfaceError::MissingDependency(format!(
-                "Command `{}` required by wireguard-rs couldn't be found, details: {}",
-                cmd, err
-            ))
-        })?;
-        debug!("Command `{}` is available", cmd);
-    }
+    let paths = env::var_os("PATH").ok_or(WireguardInterfaceError::MissingDependency(
+        "Environment variable `PATH` not found".into(),
+    ))?;
 
-    Ok(())
+    // Find the missing command to provide a more informative error message later
+    let missing = COMMANDS.iter().find(|cmd| {
+        env::split_paths(&paths)
+            .find(|dir| {
+                debug!("Trying to find {cmd} in {dir:?}");
+                match dir.join(cmd).try_exists() {
+                    Ok(true) => {
+                        debug!("{cmd} found in {dir:?}");
+                        true
+                    }
+                    Ok(false) => {
+                        debug!("{cmd} not found in {dir:?}");
+                        false
+                    }
+                    Err(err) => {
+                        warn!("Error while checking for {cmd} in {dir:?}: {err}");
+                        false
+                    }
+                }
+            })
+            .is_none()
+    });
+
+    if let Some(cmd) = missing {
+        return Err(WireguardInterfaceError::MissingDependency(format!(
+            "Command `{}` required by wireguard-rs couldn't be found. The following directories were checked: {paths:?}",
+            cmd
+        )));
+    } else {
+        debug!("All commands required by wireguard-rs are available");
+        Ok(())
+    }
 }
