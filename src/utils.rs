f14728fd4386be9bb984f5d39f7606bc984aaa17
Adam Ciarciński <adam@defguard.net>
2026-01-26 11:27:27 +0100

    Fix condition

--- src/utils.rs
+++ src/utils.rs
@@ -6,7 +6,12 @@ use std::net::{Ipv4Addr, Ipv6Addr};
 use std::path::Path;
 #[cfg(target_os = "linux")]
 use std::{collections::HashSet, fs::OpenOptions};
-#[cfg(any(target_os = "freebsd", target_os = "linux", target_os = "netbsd"))]
+#[cfg(any(
+    feature = "check_dependencies",
+    target_os = "freebsd",
+    target_os = "linux",
+    target_os = "netbsd"
+))]
 use std::{io::Write, path::PathBuf, process::Stdio};
 use std::{
     io::{BufRead, BufReader},
